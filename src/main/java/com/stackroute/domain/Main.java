ac853850a41944377d7bdbaff2462e1f13c7bdfc
subpal26 <subpal26@in.ibm.com>
2019-03-03 21:52:13 +0530

    Added Project

--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -1,33 +1,30 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
-//Task 3:
-//        From the constructor-injection branch of spring-xml-demo repo create a
-//        autowire-xml branch.
-//        For the Movie bean, delete the constructor based object injection in the bean definition file
-//        (beans.xml) that injects an Actor bean.
-//        Use autowire byName in the Movie bean to inject an Actor bean.
-//        Run the application.
-//        Create another Movie bean and try autowire byType.
-//        Run the application and note the exception thrown.
-//        Fix the Movie bean by removing autowire byType and using constructor injection instead.
-//        Push the code to autowire-xml branch.
+//Task 4:
+//        From the autowire-xml branch of spring-xml-demo repo create an aware-interface
+//        branch.
+//        Implement ApplicationContextAware, BeanFactoryAware, BeanNameAware in the
+//        Movie class and print out their results.
+//        Push the code to aware-interface branch.
 
 @SuppressWarnings("deprecation")
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+     ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-        Movie m1 = (Movie) context.getBean("movie1");
-        System.out.println("Movie 1 Details : " + m1);
+     Movie m1 = context.getBean("movie1",Movie.class);
+     m1.setApplicationContext(context);
 
-        Movie m2 = (Movie) context.getBean("movie2");
-        System.out.println("Movie 2 Details : " + m2);
-
+     XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
+     m1.setBeanFactory(factory);
+     m1.setBeanName("Movie 1 Bean");
+        System.out.println("\nMovie 1 is : "+ m1);
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,7 +1,17 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
     private Actor oneActor;
+    private String beanName;
+    private ApplicationContext context;
+    private BeanFactory beanFactory;
 
     Movie(){}
 
@@ -18,10 +28,26 @@ public class Movie {
         this.oneActor = oneActor;
     }
 
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.context = context;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    public void setBeanName(String s) {
+        this.beanName = s;
+    }
+
+    public String getBeanName() {
+        return beanName;
+    }
+
     @Override
     public String toString() {
         return "Movie{" +
-                "oneActor=" + oneActor +
+                "oneActor=" + oneActor + " " + beanName +
                 '}';
     }
 }
